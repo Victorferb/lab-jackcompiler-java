66d111fb6fea3ef64b824055552a84818bfcaaad
Victorferb <victor-ferreira12@hotmail.com>
2023-05-01 01:25:59 -0300

    projeto analisador lexico

---
+++ src/main/java/br/ufma/ecp/token/TokenType.java
@@ -0,0 +1,61 @@
+package br.ufma.ecp.token;
+
+import java.util.List;
+//import java.util.Map;
+
+public enum TokenType {
+    PLUS,MINUS, SLASH,
+
+     // Literals.
+     NUMBER,
+     STRING,
+
+
+     IDENT,
+
+ 
+     // keywords
+     WHILE, CLASS,CONSTRUCTOR,FUNCTION,
+     METHOD,FIELD,STATIC,VAR,INT,
+     CHAR,BOOLEAN,VOID,TRUE,FALSE,
+     NULL,THIS,LET,DO,IF,ELSE, RETURN,
+     EOF,
+
+
+          // symbols
+    LPAREN,RPAREN,
+    LBRACE, RBRACE,
+    LBRACKET,RBRACKET,
+
+    COMMA, SEMICOLON, DOT,
+  
+    ASTERISK,
+
+    AND, OR, NOT,
+
+    LT, GT, EQ, 
+     ILLEGAL;
+
+
+
+     
+
+     static public boolean isSymbol (char c) {
+        String symbols = "{}()[].,;+-*/&|<>=~";
+        return symbols.indexOf(c) > -1;
+    }
+
+
+    static public boolean isKeyword (TokenType type) {
+        List<TokenType> keywords  = 
+            List.of(
+                METHOD,
+                WHILE,
+                IF,
+                CLASS, FIELD, INT, VOID, BOOLEAN,
+                CONSTRUCTOR
+            );
+            return keywords.contains(type);
+    }
+
+}
